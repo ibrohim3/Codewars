8c15efc1fd7fd5d6456e065f34c9caffa1a5d400
ibrohim3 <ibrohim.2128382@gmail.com>
2025-11-02 15:17:31 +0500

    done

--- 7kyu/index.js
+++ 7kyu/index.js
@@ -1,23 +1,41 @@
 // 1 [https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript]
 
-
 // Trolls are attacking your comment section!
 // A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
 // Your task is to write a function that takes a string and return a new string with all vowels removed.
 // For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 // Note: for this kata y isn't considered a vowel.
 
-
 // function disemvowel(str) {
 //     const vowels = 'aeiouAEIOU';
 //     return str.split('').filter(ch => !vowels.includes(ch)).join('');
 // }
 
-// [https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript]
+// ---------------------------------------------------------------------------
 
+// 2 [https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript]
 
+// Implement a function that adds two numbers together and returns their sum in binary.
+// The conversion can be done before, or after the addition.
+// The binary number returned should be a string.
+// Examples:(Input1, Input2 --> Output (explanation))) 
 
-function addBinary(a, b) {
-    const sum = a + b
-    return sum.toString(2)
+// function addBinary(a, b) {
+//     const sum = a + b
+//     return sum.toString(2)
+// }
+
+// ---------------------------------------------------------------------------
+
+// [https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript]
+
+\
+
+function nbYear(p0, percent, aug, p) {
+    let years = 0;
+    while (p0 < p) {
+        p0 = Math.floor(p0 + (p0 * percent / 100) + aug);
+        years++;
+    }
+    return years;
 }
