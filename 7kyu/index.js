fbe79b0f0524fb1b10617818e56abf8f1435f3f8
ibrohim3 <ibrohim.2128382@gmail.com>
2025-11-02 14:55:01 +0500

    done

---
+++ 7kyu/index.js
@@ -0,0 +1,6 @@
+// [https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript]
+
+// function disemvowel(str) {
+//     const vowels = 'aeiouAEIOU';
+//     return str.split('').filter(ch => !vowels.includes(ch)).join('');
+// }
