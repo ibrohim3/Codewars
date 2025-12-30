4a172f02c93cf2bb28927b3956b858a404b807b1
ibrohim3 <ibrohim.2128382@gmail.com>
2025-12-30 17:13:04 +0500

    done

--- 7kyu/index.js
+++ 7kyu/index.js
@@ -167,3 +167,15 @@ function sumMul(n, m) {
 
     return sum;
 }
+
+// 11 [https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript]
+
+// Task You get an array of numbers, return the sum of all of the positives ones.
+function positiveSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] > 0 ? sum += arr[i] : null;
+    }
+    return sum;
+}
+
